b74020377e0d47a639697ff11d8ee34ea5df051e
sajanrai <sajan0rai@gmail.com>
2016-03-17 13:57:50 +0545

    bootstrap not while compiling

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Todo
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.initialize_on_precompile = false
+
     config.active_record.raise_in_transactional_callbacks = true
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
